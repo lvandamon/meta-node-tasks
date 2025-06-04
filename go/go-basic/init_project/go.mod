f2d4ddb1af61acfa2bd3f0536e15654019dc712c
Damon Zhang <damon.zhang@foxmail.com>
2025-06-04 15:43:45 +0800

    01_init_project

---
+++ go/go-basic/init_project/go.mod
@@ -0,0 +1,3 @@
+module init_project
+
+go 1.24.3
---
+++ go/go-basic/init_project/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// var s string = "Hello, world"
+	// var b byte = s[0]
+	// fmt.Println(os.Args)
+	fmt.Println("Hello, world!")
+}
