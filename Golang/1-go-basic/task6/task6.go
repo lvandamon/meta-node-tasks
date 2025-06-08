d543d73418806dc888156c2f706677979e0533ac
Damon Zhang <damon.zhang@foxmail.com>
2025-06-09 01:37:34 +0800

    finish go-basic task6 task8

--- Golang/1-go-basic/task6/task6.go
+++ Golang/1-go-basic/task6/task6.go
@@ -1,20 +1,47 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func plusOne(digits []int) []int {
-	var result []int
-	//TODO
+	n := len(digits)
 
-	return result
+	// 从最后一位开始处理
+	for i := n - 1; i >= 0; i-- {
+		// 当前位加1
+		digits[i]++
+
+		// 如果加1后小于10，直接返回
+		if digits[i] < 10 {
+			return digits
+		}
+
+		// 否则当前位设为0，进位到前一位
+		digits[i] = 0
+	}
+
+	// 如果所有位都进位了（例如999变成1000的情况）
+	// 需要在最前面添加一个1
+	return append([]int{1}, digits...)
 }
 
 func main() {
-	fmt.Println(plusOne([]int{9}))
-	fmt.Println(plusOne([]int{1, 9}))
-	fmt.Println(plusOne([]int{1, 2, 3}))
-	fmt.Println(plusOne([]int{4, 3, 2, 1}))
+	// 测试用例1
+	digits1 := []int{1, 2, 3}
+	fmt.Println(plusOne(digits1)) // 输出: [1 2 4]
 
+	// 测试用例2
+	digits2 := []int{4, 3, 2, 1}
+	fmt.Println(plusOne(digits2)) // 输出: [4 3 2 2]
+
+	// 测试用例3
+	digits3 := []int{9}
+	fmt.Println(plusOne(digits3)) // 输出: [1 0]
+
+	// 测试用例4 - 多个9的情况
+	digits4 := []int{9, 9, 9}
+	fmt.Println(plusOne(digits4)) // 输出: [1 0 0 0]
+
+	// 测试用例5 - 0的情况
+	digits5 := []int{0}
+	fmt.Println(plusOne(digits5)) // 输出: [1]
 }
--- Golang/1-go-basic/task8/task8.go
+++ Golang/1-go-basic/task8/task8.go
@@ -1,16 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func merge(intervals [][]int) [][]int {
-	//TODO
-	return [][]int{}
+	// 如果区间数量小于等于1，直接返回
+	if len(intervals) <= 1 {
+		return intervals
+	}
+
+	// 先按照区间的起始位置排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	// 初始化结果集，先把第一个区间加入
+	merged := [][]int{intervals[0]}
+
+	for i := 1; i < len(intervals); i++ {
+		last := merged[len(merged)-1]
+		current := intervals[i]
+
+		// 如果当前区间的起始位置 <= 上一个区间的结束位置，说明有重叠
+		if current[0] <= last[1] {
+			// 合并区间，结束位置取两者中较大的
+			if current[1] > last[1] {
+				last[1] = current[1]
+			}
+		} else {
+			// 没有重叠，直接加入结果集
+			merged = append(merged, current)
+		}
+	}
+
+	return merged
 }
 
 func main() {
+	// 测试用例1
 	intervals1 := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
-	fmt.Println(merge(intervals1))
+	fmt.Println(merge(intervals1)) // 输出: [[1 6] [8 10] [15 18]]
 
+	// 测试用例2
 	intervals2 := [][]int{{1, 4}, {4, 5}}
-	fmt.Println(merge(intervals2))
+	fmt.Println(merge(intervals2)) // 输出: [[1 5]]
+
+	// 测试用例3 - 完全包含的情况
+	intervals3 := [][]int{{1, 4}, {2, 3}}
+	fmt.Println(merge(intervals3)) // 输出: [[1 4]]
+
+	// 测试用例4 - 空输入
+	intervals4 := [][]int{}
+	fmt.Println(merge(intervals4)) // 输出: []
+
+	// 测试用例5 - 单个区间
+	intervals5 := [][]int{{1, 3}}
+	fmt.Println(merge(intervals5)) // 输出: [[1 3]]
 }
